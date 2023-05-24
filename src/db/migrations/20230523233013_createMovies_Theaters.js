7e3ba270296ed6837be0b639ea19f13ff99f1330
Sarah Shu <sarahjadeshu@gmail.com>
2023-05-24 00:19:27 -0700

    third commit

--- src/db/migrations/20230523233013_createMovies_Theaters.js
+++ src/db/migrations/20230523233013_createMovies_Theaters.js
@@ -1,8 +1,23 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("movies_theaters", (table) => {
+    table.integer("movies_id").unsigned().notNullable();
+    table
+      .foreign("movie_id")
+      .references("movie_id")
+      .inTable("movies")
+      .onDelete("CASCADE");
 
-exports.up = function(knex) {
-  
+    table.integer("theater_id").unsigned().notNullable();
+    table
+      .foreign("theater_id")
+      .references("movie_id")
+      .inTable("theaters")
+      .onDelete("CASCADE");
+
+    table.boolean("is_showing");
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("movies_theaters");
 };

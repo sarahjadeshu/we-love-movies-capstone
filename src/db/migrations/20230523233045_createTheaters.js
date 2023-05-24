2381688e9760aad5bb072fcead48804a554b05af
Sarah Shu <sarahjadeshu@gmail.com>
2023-05-24 00:32:01 -0700

    fourth commit

--- src/db/migrations/20230523233045_createTheaters.js
+++ src/db/migrations/20230523233045_createTheaters.js
@@ -1,8 +1,17 @@
 
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("theaters", (table) => {
+    table.increments("theater_id").primary();
+    table.string("name");
+    table.string("address_line_1");
+    table.string("address_line_2");
+    table.string("city");
+    table.string("state");
+    table.zip("97212");
+    table.timestamps(true, true);
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTable("theaters");
 };
